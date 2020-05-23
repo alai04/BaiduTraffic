3f7f1c0a5c6a1db7318df480e3a55b27618ec4ad
alai04 <alai04@gmail.com>
2020-05-23 10:44:01 +0800

    add: loop for getmap, per minute

--- crawling/main.go
+++ crawling/main.go
@@ -2,18 +2,65 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"time"
 )
 
+type config struct {
+	test    bool
+	level   int
+	workers int
+}
+
 func main() {
-	level := flag.Int("l", 9, "level of map")
-	workers := flag.Int("w", 1, "num of workers")
+	cfg := parse()
+	if cfg.test {
+		test(cfg)
+	} else {
+		fmt.Println("Run as deamon, Crtl-C to exit ...")
+		loop()
+	}
+}
+
+func parse() config {
+	var cfg config
+	flag.BoolVar(&cfg.test, "t", false, "test mode")
+	flag.IntVar(&cfg.level, "l", 9, "level of map")
+	flag.IntVar(&cfg.workers, "w", 1, "num of workers")
 	flag.Parse()
-	m := NewTrafficMap(*level, *workers)
+	return cfg
+}
+
+func test(cfg config) {
+	m := NewTrafficMap(cfg.level, cfg.workers)
 	fn, err := m.GetMap()
 	if err == nil {
-		log.Printf("Traffic map save to: %v\n", fn)
+		log.Printf("Traffic map save to: %v", fn)
 	} else {
 		log.Printf("GetTrafficMap() error: %v", err)
+	}
+}
+
+func loop() {
+	workers := 10
+	for {
+		tBegin := time.Now()
+		tNext := tBegin.Add(time.Minute)
+		for l := 9; l < 13; l++ {
+			m := NewTrafficMap(l, workers)
+			fn, err := m.GetMap()
+			if err == nil {
+				log.Printf("Traffic map save to: %v", fn)
+			} else {
+				log.Printf("GetTrafficMap() error: %v", err)
+			}
+		}
+		log.Printf("Loop consuming: %v", time.Since(tBegin))
+		if time.Now().Before(tNext) {
+			d := time.Until(tNext)
+			log.Printf("Wait %v for next loop", d)
+			time.Sleep(d)
+		}
 	}
 }
