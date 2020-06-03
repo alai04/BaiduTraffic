42ba66fb62296af0d27b985e087d03ae715b2e5c
alai04 <alai04@gmail.com>
2020-06-03 18:47:35 +0800

    minor fix

--- adjust/metrics.go
+++ adjust/metrics.go
@@ -12,15 +12,8 @@ import (
 
 // metrics:
 // adjust_maps_total  调整的图片总数，标签:level
-// adjust_consuming_total_seconds  总耗时，标签:level
-// adjust_consuming_average_seconds  平均耗时，标签:level
-// adjust_consuming_min_seconds  最小耗时，标签:level
-// adjust_consuming_max_seconds  最大耗时，标签:level
-// adjust_maps_average_bytes  图片平均大小，标签:level
-// adjust_maps_min_bytes  图片最小大小，标签:level
-// adjust_maps_max_bytes  图片最大大小，标签:level
 // adjust_maps_failure  调整失败的图片数，标签:level,stage
-// adjust_succ_precent  调整成功的比例，标签:level
+// adjust_consuming_total_seconds  总耗时，标签:level
 
 const subsystem = "adjust"
 
@@ -34,7 +27,7 @@ var (
 		Subsystem: subsystem,
 		Name:      "maps_failure",
 		Help:      "调整失败的图片数",
-	}, []string{"level"})
+	}, []string{"level", "stage"})
 	adjustConsumingTotalSeconds = promauto.NewSummaryVec(prometheus.SummaryOpts{
 		Subsystem: subsystem,
 		Name:      "consuming_total_seconds",
@@ -42,7 +35,16 @@ var (
 	}, []string{"level"})
 )
 
+func init() {
+	prometheus.MustRegister(adjustMapsTotal)
+	prometheus.MustRegister(adjustMapsFailure)
+	prometheus.MustRegister(adjustConsumingTotalSeconds)
+}
+
 func metricsServe(port string) {
+	// Initial some counter
+	adjustMapsFailure.With(prometheus.Labels{"level": "9", "stage": "OPEN"}).Add(0)
+
 	http.Handle("/metrics", promhttp.Handler())
 	log.Printf("Starting metrics server at http://localhost:%s/metrics", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
